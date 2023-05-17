70bca7d133cd70725e82726e1ce33dbf62b6dd54
Nathan Koga <nathan.koga@gmail.com>
2023-05-16 18:36:33 -0700

    UI: Added Event for disabling tutorial text upon move.

--- Assets/Source/Dungeon/DungeonGUI.cs
+++ Assets/Source/Dungeon/DungeonGUI.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DungeonGUI : MonoBehaviour, IGUIManager
 {
     /*
      * This class manages the overworld GUI while exploring dungeons.
      */
-
+    public PlayerLogic player;
+    public TextMeshProUGUI tutorialText;
     public Canvas canvas;
 
+    
+    public void Awake(){
+        PlayerLogic.MoveUpdate += RemoveTutorialText;
+    }
+    
+    // remove the "wasd to move text"
+    public void RemoveTutorialText(){
+        tutorialText.text = "";
+    }
+    
+    
     public void EnableGUI()
     {
         canvas.enabled = true;
