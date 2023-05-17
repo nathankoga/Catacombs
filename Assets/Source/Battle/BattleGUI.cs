10742c5d5fb8871d113ce70f4b29b2a852fa4e9d
Nathan Koga <nathan.koga@gmail.com>
2023-05-16 22:43:11 -0700

    UI: Added basic implementation of battle UI

--- Assets/Source/Battle/BattleGUI.cs
+++ Assets/Source/Battle/BattleGUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BattleGUI : MonoBehaviour, IGUIManager
 {
@@ -8,10 +9,23 @@ public class BattleGUI : MonoBehaviour, IGUIManager
      * This class manages the interface for the battle system.
      */
 
+    public BattleManager battleManager;
+    public TextMeshProUGUI battleStatsText;
+    public TextMeshProUGUI battleAbilitiesText;
     public Canvas canvas;
+
+    public void Awake(){
+        BattleManager.uiBattleUpdate += updateStats;
+    }
+
+    public void updateStats(string[] stats){
+        battleStatsText.text = stats[0] + "\n" + stats[1] + "\n" + stats[2] + "\n" + stats[3];
+        battleAbilitiesText.text = stats[4];
+    }
 
     public void EnableGUI()
     {
+       // NOTE: deleted the original "press space to kill" prompt from the canvas (in game hierarchy) 
         canvas.enabled = true;
     }
 
